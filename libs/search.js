cbfd5d013edc00b5b42b01535d15989543f7bf79
Александр Иванов <a.ivanov@reg.ru>
2018-04-27 12:17:13 +0300

    Add nounique option to glob search: now search matches should be returned in order of paths in levels

--- libs/search.js
+++ libs/search.js
@@ -31,7 +31,7 @@ module.exports = function(context, entity, options) {
 
         const pattern = require('./pattern')( entity, options );
 
-        new glob.Glob(pattern, { nosort: true }, function(err, matches) {
+        new glob.Glob(pattern, { nosort: true, nounique: true }, function(err, matches) {
             const deduplicated = deduplicateMatches(matches);
 
             if (err) {
